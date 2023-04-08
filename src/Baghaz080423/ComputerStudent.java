003881df497e35583ba0811ae9c0de3b4ff0c21d
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2023-04-08 14:14:21 +0700

    menambahkan pemanggilan untuk latihan 1 module 11

--- src/Baghaz080423/ComputerStudent.java
+++ src/Baghaz080423/ComputerStudent.java
@@ -27,4 +27,17 @@ public class ComputerStudent extends StudentRecord{
         return (getMathGrade() + getEnglishGrade() + getScienceGrade()+getCompGrade())/4;
         //untuk memeanggil variabel average, kita harus merubah public menjadi protected
     }
+    
+    //pemanggilan
+    public static void main(String[] args){
+        ComputerStudent siA = new ComputerStudent();
+        siA.setName("Hamba Allah");
+        siA.setCompGrade(90);
+        siA.setEnglishGrade(80);
+        siA.setMathGrade(90);
+        siA.setScienceGrade(80);
+        System.out.println("Nama  : "+siA.getName());
+        System.out.println("Rata2 : "+siA.getAverage());
+        System.out.println("Nilai : "+siA.nilaiHuruf());
+    }
 }
